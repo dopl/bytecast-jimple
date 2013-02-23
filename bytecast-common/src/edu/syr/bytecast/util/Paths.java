ee7b2d03724067b89b4408a25e53edf704107b60
Shawn Mueller <smueller@syr.edu>
2013-02-23 17:15:01 -0500

    Added path to the mock1 input for bytecast fsys to Paths.java MockBytecastFsys can now read the mock1 input for fsys

--- bytecast-common/src/edu/syr/bytecast/util/Paths.java
+++ bytecast-common/src/edu/syr/bytecast/util/Paths.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class Paths {
     
     private static Paths m_instance;
-    private static final String m_FsysMock1Path = "";
-    
+    private static final String m_fsysMock1Path = "bytecast-common/bytecast-common/test_input_files/fsys_mock1.eobj";
+    private String m_root;
     public static synchronized Paths v(){
         if(m_instance == null){
             m_instance = new Paths();
@@ -14,21 +14,20 @@ public class Paths {
         return m_instance;
       
     }
-    String m_absolute_root;
     public void setRoot(String root_dir)
     {
         File new_file = new File(root_dir);
-        m_absolute_root = new_file.getAbsolutePath();
+        m_root = new_file.getAbsolutePath();
     }
     
     public String getRoot()
     {
-        return m_absolute_root;
+        return m_root;
     }
     
     public String getFsysMock1Path()
     {
-        return m_FsysMock1Path;
+        return m_root + "/" + m_fsysMock1Path;
     }
     
 }
