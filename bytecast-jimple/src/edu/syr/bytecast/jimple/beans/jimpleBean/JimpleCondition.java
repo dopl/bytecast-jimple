4323b7588ea78f3fc15982883241ec64dbd52418
Peike Dai <dpk1216@gmail.com>
2013-03-09 14:35:59 -0500

    add some code

--- bytecast-jimple/src/edu/syr/bytecast/jimple/beans/jimpleBean/JimpleCondition.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/beans/jimpleBean/JimpleCondition.java
@@ -10,6 +10,18 @@ package edu.syr.bytecast.jimple.beans.jimpleBean;
  */
 public class JimpleCondition extends JimpleElement{
     private String type; // if or switch
+    private String comparator; // < > <= >= or ==
+    
+    public JimpleCondition(String type) {
+        this(type,null);
+    }
+    
+    public JimpleCondition(String type, String comprt) {
+        this.type = type;
+        
+        this.comparator = comprt != null ? comprt : null;
+    }
+    
     
     
     
--- bytecast-jimple/src/edu/syr/bytecast/jimple/beans/jimpleBean/JimpleElement.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/beans/jimpleBean/JimpleElement.java
@@ -4,10 +4,28 @@
  */
 package edu.syr.bytecast.jimple.beans.jimpleBean;
 
+import soot.Local;
+import soot.RefType;
+import soot.Unit;
+import soot.jimple.Jimple;
+
 /**
  *
  * @author Peike Dai
  */
 public abstract class JimpleElement {
+    private Local asParameter;
     
+    
+    public abstract Unit getElement();
+    
+    public void setParameter(String name, String type) {
+        this.asParameter = Jimple.v().newLocal(name, RefType.v("java.io.PrintStream"));
+    }
+    
+    public Local getParameter() {
+        if (asParameter != null)
+            return asParameter;
+        else return null;
+    }
 }
