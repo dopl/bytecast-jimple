610cdf7c1e1cf3bc14c653c9c717b2c40061053f
Qazi Sameer Ahmed <qsameer@gmail.com>
2013-03-09 17:22:45 -0500

    addedscanner

---
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/impl/ConfigScanner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.syr.bytecast.jimple.impl;
+
+/**
+ *
+ * @author QSA
+ */
+public class ConfigScanner {
+    public void config()
+    {
+        
+    }
+}
