3dd969b2d915d82962e50e40aa88dbeb4153bbed
Xirui Wang <xwang74@syr.edu>
2013-03-23 11:45:50 -0400

    test file

--- bytecast-jimple/src/edu/syr/bytecast/jimple/beans/jimpleBean/TestJimple.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/beans/jimpleBean/TestJimple.java
@@ -6,8 +6,44 @@ package edu.syr.bytecast.jimple.beans.jimpleBean;
 
 /**
  *
- * @author Peike Dai
+ * @author xirui Wang
  */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import soot.*;
+import soot.jimple.IdentityStmt;
+import soot.jimple.IntConstant;
+import soot.jimple.JasminClass;
+import soot.jimple.Jimple;
+import soot.jimple.JimpleBody;
+import soot.jimple.StringConstant;
+import soot.options.Options;
+import soot.util.Chain;
+
 public class TestJimple {
+    
+    void doTest(){
+          //create a class          
+          //declare function
+          //declare variable
+          //assign variable
+          //
+        JimpleDoc  jDoc = new JimpleDoc();
+        
+        JimpleClass jClass = new JimpleClass("test", 1); 
+        jClass.createClass("test", 1);
+        
+        ArrayList<String>  parameter_list = new  ArrayList<String>();
+        parameter_list.add("int");
+        parameter_list.add("int");
+        
+        JimpleMethod jMethod = new JimpleMethod("sum", "int", jClass, 1,parameter_list);
+        jMethod.createMethod();
+        
+        
+    }
+    
     
 }
