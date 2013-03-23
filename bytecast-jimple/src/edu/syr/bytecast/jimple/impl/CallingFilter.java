73f9539c66d4d84b0bcc9fce131bc287e4f2ba53
Fei Qi <qifei1989@126.com>
2013-03-23 17:44:32 -0400

    progress in the filter

--- bytecast-jimple/src/edu/syr/bytecast/jimple/impl/CallingFilter.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/impl/CallingFilter.java
@@ -21,9 +21,10 @@ package edu.syr.bytecast.jimple.impl;
 import edu.syr.bytecast.amd64.api.constants.InstructionType;
 import edu.syr.bytecast.amd64.api.constants.OperandType;
 import edu.syr.bytecast.amd64.api.instruction.IInstruction;
-import edu.syr.bytecast.jimple.api.AbstractFilter;
+import edu.syr.bytecast.jimple.beans.jimpleBean.ParsedInstructionsSet;
+import edu.syr.bytecast.jimple.beans.jimpleBean.JInstructionInfo;
 import edu.syr.bytecast.jimple.api.IFilter;
-import edu.syr.bytecast.jimple.beans.ParsedInstructionsSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -36,24 +37,10 @@ public class CallingFilter implements IFilter{
     {
         IInstruction ins = instList.get(index);
         if( ins.getInstructiontype() == InstructionType.CALLQ 
-                && ins.getOperands().get(0).getOperandType() == OperandType.MEMORY_PHYSICAL_ADDRESS
-                && ins.getOperands().get(1).getOperandValue() == OperandType.MEMORY_PHYSICAL_ADDRESS )
+                && ins.getOperands().get(0).getOperandType() == OperandType.MEMORY_PHYSICAL_ADDRESS )
                 //the value of the second operand should be the SectionName, it hasn't been set up in AMD64 api
         {
             return true;
-        }
-        //calling with parameter
-        if( ins.getInstructiontype() == InstructionType.MOV
-                && ins.getOperands().get(0).getOperandType() == OperandType.CONSTANT
-                && ins.getOperands().get(1).getOperandValue() == OperandType.REGISTER )
-        {
-            ins = instList.get(index + 1);
-            if( ins.getInstructiontype() == InstructionType.CALLQ
-                    && ins.getOperands().get(0).getOperandType() == OperandType.MEMORY_PHYSICAL_ADDRESS
-                    && ins.getOperands().get(1).getOperandValue() == OperandType.MEMORY_PHYSICAL_ADDRESS ) //SectionName
-            {
-                return true;
-            }
         }
         return false;
     } 
--- bytecast-jimple/src/edu/syr/bytecast/jimple/impl/SetArgvAndArgcFilter.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/impl/SetArgvAndArgcFilter.java
@@ -7,7 +7,8 @@ package edu.syr.bytecast.jimple.impl;
 import edu.syr.bytecast.amd64.api.constants.InstructionType;
 import edu.syr.bytecast.amd64.api.constants.OperandType;
 import edu.syr.bytecast.amd64.api.instruction.IInstruction;
-import edu.syr.bytecast.jimple.api.AbstractFilter;
+import edu.syr.bytecast.jimple.beans.jimpleBean.ParsedInstructionsSet;
+import edu.syr.bytecast.jimple.beans.jimpleBean.JInstructionInfo;
 import edu.syr.bytecast.jimple.api.IFilter;
 import java.util.List;
 import java.util.Map;
@@ -18,28 +19,34 @@ import java.util.Map;
  */
 //this file is used to filter the pre-memory process part
 
-public class SetArgumentFilter extends AbstractFilter implements IFilter{
+public class SetArgvAndArgcFilter implements IFilter{
     public boolean doTest(Map<Long, IInstruction> instList, int index)
     {
         IInstruction ins = instList.get(index);
+        int count = 0;
         if( ins.getInstructiontype() == InstructionType.MOV
                 && ins.getOperands().get(0).getOperandType() == OperandType.REGISTER
                 && ins.getOperands().get(0).getOperandValue() == "%edi"
                 //&& ins.getOperands().get(1).getOperandType() == OperandType.MEMORY_ADDRESS
-                && ins.getOperands().get(1).getOperandValue().toString().contains("%rbp"))
+                && ins.getOperands().get(1).getOperandValue().toString().contains("%rbp") )
         {
-            ins = instList.get(index + 1);
+            count++;
+            ins = instList.get(index + count);
             if( ins.getInstructiontype() == InstructionType.MOV
                     && ins.getOperands().get(0).getOperandType() == OperandType.REGISTER
                     && ins.getOperands().get(0).getOperandValue() == "%rsi"
                     //&& ins.getOperands().get(1).getOperandType() == OperandType.MEMORY_ADDRESS
                     && ins.getOperands().get(1).getOperandValue().toString().contains("%rbp")) //SectionName
             {
+                count++;
                 return true;
+                }
             }
-        }
         return false;
-    }
-    
-    
+    } 
 }
+
+
+
+    
+    
