e174aad48e6761cba1ea51f7e7d0e277c5b11e40
CollinDai <dpk1216@gmail.com>
2013-04-14 11:48:22 -0400

    more code

--- bytecast-jimple/src/edu/syr/bytecast/jimple/impl/PatternSeperator.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/impl/PatternSeperator.java
@@ -4,18 +4,17 @@
  */
 package edu.syr.bytecast.jimple.impl;
 
-import edu.syr.bytecast.amd64.api.constants.InstructionType;
-import edu.syr.bytecast.amd64.api.constants.OperandType;
 import edu.syr.bytecast.amd64.api.instruction.IInstruction;
 import edu.syr.bytecast.amd64.api.output.ISection;
 import edu.syr.bytecast.amd64.api.output.MemoryInstructionPair;
 import edu.syr.bytecast.jimple.api.IFilter;
+import edu.syr.bytecast.jimple.api.MethodInfo;
 import edu.syr.bytecast.jimple.beans.*;
+import edu.syr.bytecast.jimple.api.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  *
@@ -24,59 +23,80 @@ import java.util.Set;
 public class PatternSeperator {
     // the result that store the filtered section
     private Map<ISection, List<ParsedInstructionsSet>> result;
+    private MethodInfo methodInfo;
 //    // store the temperate result of each ParsedInstructionsSet
 //    private List<ParsedInstructionsSet> temp_set_result;
     // used to store the all section name in the file and in order to judge whether the section is analyzed
-    private Map<String, Boolean> name_of_section;
+    //private Map<String, Boolean> name_of_section;
     
-    public Map<ISection, List<ParsedInstructionsSet>> doFilter(List<ISection> all_section)
+    private List<Method> divideMethod(ISection wholeSection) {
+      List<MemoryInstructionPair> mip = wholeSection.getAllInstructionObjects();
+      for(MethodInfo m_info : methodInfo)
+      {
+        Method m = new Method();
+        m.setMethodInfo(m_info);
+        for(m_info.getStartMemeAddress();)
+      }
+      return
+    }
+    
+    public Map<ISection, List<ParsedInstructionsSet>> doFilter(ISection wholeSection, List<MethodInfo> m_info)
     {
         // the objective section to be filtered
-        ISection obj_sec = all_section.get(0);
+//        ISection obj_sec = all_section.get(0);
         result = new HashMap<ISection, List<ParsedInstructionsSet>>();
-//        temp_set_result = new ArrayList<ParsedInstructionsSet>();
-        name_of_section = new HashMap<String, Boolean>();
-        
-        // find the main section
-        for(int i = 0; i < all_section.size(); i++)
-        {
-            if(all_section.get(i).getSectionName().equals("main"))
-            {
-                obj_sec = all_section.get(i);
-                break;
-            }
-        }
+        List<Method> methods = divideMethod(wholeSection);
+        for (MethodInfo methodinfo : m_info) {
+//          for (ISection section : all_section) {
+            String sectionname = wholeSection.getSectionName();
+            if (sectionname.equals(methodinfo.getMethodName())) {
+              result.put(section, analyze(wholeSection));
+//            }
+          }
+      }
+////        temp_set_result = new ArrayList<ParsedInstructionsSet>();
+//        name_of_section = new HashMap<String, Boolean>();
+//        
+//        // find the main section
+//        for(int i = 0; i < all_section.size(); i++)
+//        {
+//            if(all_section.get(i).getSectionName().equals("main"))
+//            {
+//                obj_sec = all_section.get(i);
+//                break;
+//            }
+//        }
         // filter the main function 
         // the name_of_section record the section name needed to be filtered
-        result.put(obj_sec, analyze(obj_sec));
+        
         
         // filter the subfunction
         
-        while(true)
-        {
-            // get all names of analyzed section
-            Set<String> names = name_of_section.keySet();
-            int judge = 0;
-            for (String str : names) {
-                if(name_of_section.get(str) == Boolean.FALSE){
-                    name_of_section.put(str, Boolean.TRUE);
-                    judge = 1;
-                    for(int i = 0; i < all_section.size(); i++)
-                    {
-                        if(all_section.get(i).getSectionName().equals(str))
-                        {
-                            // find the objective section
-                            obj_sec = all_section.get(i);
-                            break;
-                        }
-                    }
-                    // filter the section
-                    result.put(obj_sec, analyze(obj_sec));
-                }
-            }
-            if( judge == 0)
-                break;
-        }
+//        while(true)
+//        {
+//            // get all names of analyzed section
+//            Set<String> names = name_of_section.keySet();
+//            int judge = 0;
+//            for (String str : names) {
+//                if(name_of_section.get(str) == Boolean.FALSE){
+//                    name_of_section.put(str, Boolean.TRUE);
+//                    judge = 1;
+//                    for(int i = 0; i < all_section.size(); i++)
+//                    {
+//                        if(all_section.get(i).getSectionName().equals(str))
+//                        {
+//                            // find the objective section
+//                            obj_sec = all_section.get(i);
+//                            break;
+//                        }
+//                    }
+//                    // filter the section
+//                    result.put(obj_sec, analyze(obj_sec));
+//                }
+//            }
+//            if( judge == 0)
+//                break;
+//        }
         // return the filtered result
         return result;
     }
