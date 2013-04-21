b738f31c839e913de332b219f7211b6f9cec14c0
Xirui Wang <xwang74@syr.edu>
2013-04-21 14:53:33 -0400

    little change

--- bytecast-jimple/src/edu/syr/bytecast/jimple/impl/TestStep2.java
+++ bytecast-jimple/src/edu/syr/bytecast/jimple/impl/TestStep2.java
@@ -23,6 +23,7 @@ import edu.syr.bytecast.jimple.beans.*;
 import edu.syr.bytecast.jimple.beans.jimpleBean.JimpleClass;
 import edu.syr.bytecast.jimple.beans.jimpleBean.JimpleDoc;
 import edu.syr.bytecast.jimple.beans.jimpleBean.JimpleMethod;
+import edu.syr.bytecast.jimple.beans.jimpleBean.JimpleVariable;
 import edu.syr.bytecast.util.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -37,12 +38,14 @@ public class TestStep2 {
     private JimpleDoc jimple_doc;
     private JimpleClass jimple_class;
     private Set<Method> methods;
+    private int _vcount;
     
     // maintain a relation between variable and register
     // example:
     // edx : v1
     // -0x4(%rbp) : p1
     private Map<String, String> regToVar;
+    private Map<String, JimpleVariable>  varToJVar;
     
     public TestStep2(Map<Method, List<ParsedInstructionsSet>> temp_map) {
         this.method_map = temp_map;
@@ -51,7 +54,9 @@ public class TestStep2 {
         jimple_doc = new JimpleDoc();
         jimple_class = new JimpleClass("test2", 1);
         jimple_doc.addClass(jimple_class);
+        this._vcount = 0;
         regToVar = new HashMap<String, String>();
+        varToJVar = new HashMap<String , JimpleVariable>();
     }
 
     public void createJimple() {
@@ -71,20 +76,26 @@ public class TestStep2 {
         //create all jimple method
         for (Method m : methods) {
 
+              //main method
+           if(m.getMethodInfo().getMethodName().equals("main")){
             MethodInfo m_info = m.getMethodInfo();
-            ArrayList<String> parameter_type_sum = new ArrayList<String>();
+            ArrayList<String> parameter_type = new ArrayList<String>();
+              parameter_type.add("String[]");
+           }
+            
+           else{
+            MethodInfo m_info = m.getMethodInfo();
+            ArrayList<String> parameter_type = new ArrayList<String>();
             int para_num = m_info.getParameterCount();
             if (para_num != 0) {
                 for (int i = 0; i < para_num; i++) {
-                    parameter_type_sum.add("int");
+                    parameter_type.add("int");
                 }
             }
-            JimpleMethod jMethod = new JimpleMethod(1, "int", m_info.getMethodName(), parameter_type_sum, jimple_class);
-
-
+            JimpleMethod jMethod = new JimpleMethod(1, "int", m_info.getMethodName(), parameter_type, this.jimple_class);
             //test for method output
-
             Map_jMethod.put(m_info.getMethodName(), jMethod);
+           }
         }
 //        try {
 //            jimple_doc.printJimple(jimple_class.getJClassName(), "jimple");
@@ -105,6 +116,14 @@ public class TestStep2 {
         }
     }
 
+    private int getVcount(){
+        
+        int temp = _vcount++;
+        
+        return temp;
+    }
+    
+    
     private void implementSingleJimpleMethod(Method m, List<ParsedInstructionsSet> listPis) {
         int start_index_of_this, number_of_lines;
         int end_index_of_last = 0;
@@ -255,10 +274,17 @@ public class TestStep2 {
 
     private void setArgFilterProcess(Method m, ParsedInstructionsSet ins_set) {
         List<MemoryInstructionPair> pair_list = ins_set.getInstructions_List(); 
-        String argc = pair_list.get(0).getInstruction().getOperands().get(1).getOperandValue().toString();
+        //String argc = pair_list.get(0).getInstruction().getOperands().get(1).getOperandValue().toString();
         String argv = pair_list.get(1).getInstruction().getOperands().get(1).getOperandValue().toString();
-            updateRegToVarMap(argv, argv);
-            updateRegToVarMap(argv, argv);
+            updateRegToVarMap(argv, "argv");
+            
+       //  JimpleMethod jmethod = Map_jMethod.get(m.getMethodInfo().getMethodName()); 
+        // jmethod.
+     
+            
+         
+            
+            //updateRegToVarMap(argv, argv);
     }
 
     private void useArgFilterProcess(Method m, ParsedInstructionsSet ins_set) {
@@ -295,7 +321,7 @@ public class TestStep2 {
     private boolean updateRegToVarMap(String regName, String varName) {
       //  rax is equal to eax
       if (regToVar.containsKey(regName)) {
-        regToVar.put(regName, varName);
+        regToVar.put(regName, varName);                  
       } else {
         
       }
